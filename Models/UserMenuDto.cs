3f13971717aeff78bb7e97431de2200291cc1bfa
Dang Quang Hai <dangquanghai123@gmail.com>
2026-04-03 14:08:34 +0700

    update menu

--- Models/UserMenuDto.cs
+++ Models/UserMenuDto.cs
@@ -4,9 +4,12 @@ namespace SmartSam.Models
 {
     public class UserMenuDto
     {
+        public string ModuleName { get; set; }
+        public string ModuleIcon { get; set; }
         public string FunctionID { get; set; }
         public string FunctionName { get; set; }
+        public string FunctionIcon { get; set; }
         public string Url { get; set; }
-        public string ModuleName { get; set; }
+        
     }
 }
